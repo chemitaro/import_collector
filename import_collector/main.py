2ef1a5367c68822763b3060ba080d7abbb67161f
chemitaro <iwasi_44@hotmail.com>
2023-07-10 11:14:19 +0900

    クラスに集約

--- import_collector/main.py
+++ import_collector/main.py
@@ -80,13 +80,13 @@ def get_modules_in_package(package_name: str) -> List[str]:
     return [name for _, name, _ in pkgutil.iter_modules([package_name])]
 
 
-def get_module_if_contains(package_name: str, target_class_or_func: str) -> List[str]:
+def get_module_if_contains(package_name: str, target_class_or_func_names: List[str]) -> List[str]:
     """
     指定されたパッケージに含まれるモジュールのうち、指定されたクラスまたは関数を含むモジュールの名前のリストを返します。
 
     Args:
         package_name (str): パッケージ名。
-        target_class_or_func (str): 検索するクラスまたは関数の名前。
+        target_class_or_func_names (List[str]): 検索対象のクラスまたは関数の名前。
 
     Returns:
         List[str]: 指定されたクラスまたは関数を含むモジュールの名前のリスト。
@@ -95,59 +95,11 @@ def get_module_if_contains(package_name: str, target_class_or_func: str) -> List
     for _, module_name, _ in pkgutil.iter_modules([package_name]):
         module = importlib.import_module(f"{package_name}.{module_name}")
         for name, _ in inspect.getmembers(module):
-            if name == target_class_or_func:
-                modules.append(module.__name__)
-                break
+            for target_class_or_func_name in target_class_or_func_names:
+                if name == target_class_or_func_name:
+                    modules.append(module.__name__)
+                    break
     return modules
-
-
-def extract_imports(root_path: str, relative_path: str) -> List[str]:
-    """
-    指定されたPythonファイルのインポートを解析し、インポートされたモジュールのファイルの相対パスのリストを返します。
-
-    Args:
-        root_path (str): 依存関係を解析する起点のPythonファイルのパス。
-        relative_path (str): 依存関係を解析するPythonファイルのパス。
-
-    Returns:
-        List[str]: インポートされたモジュールのファイルの相対パスのリスト。
-    """
-    # ファイルの内容を読み込む
-    code = read_file(relative_path, remove_comments=False)
-    absolute_path = os.path.join(root_path, relative_path)
-    # コードをASTで解析する
-    tree = ast.parse(code)
-
-    # AST内のすべてのインポートノードを検索する
-    imports = [node for node in ast.walk(tree) if isinstance(node, (ast.Import, ast.ImportFrom))]
-
-    # モジュールのインポートをファイルパスに変換する
-    absolute_paths = []
-
-    # モジュール名を取得する
-    for node in imports:
-        if isinstance(node, ast.Import):
-            for alias in node.names:
-                module_name = alias.name
-        elif isinstance(node, ast.ImportFrom):
-            module_name = node.module
-
-        # 相対インポートの場合は、絶対インポートに変換する
-        if isinstance(node, ast.ImportFrom) and node.level > 0:
-            base_dir = os.path.dirname(absolute_path)
-            for _ in range(node.level - 1):
-                base_dir = os.path.dirname(base_dir)
-
-            # root_pathとbase_dirの共通部分を削除するし、相対パスに変換する
-            package_relative_name = os.path.relpath(base_dir, root_path).replace("/", ".")
-            # モジュール名を結合する
-            module_name = f"{package_relative_name}.{module_name}"
-
-        module_file_path = os.path.join(root_dir, module_name.replace(".", "/")) + ".py"
-        absolute_paths.append(module_file_path)
-    # 絶対パスを相対パスに変換する
-    relative_paths = [os.path.relpath(absolute_path, root_path) for absolute_path in absolute_paths]
-    return relative_paths
 
 
 def find_file(files, file):
@@ -191,43 +143,6 @@ def exclude_paths(all_py_paths: List[str], excludes: List[str] = []) -> List[str
             all_py_paths.remove(exclude)
 
     return all_py_paths
-
-
-# 起点となるファイルのパスから、依存関係を解析して、ファイルのパスを返す
-def search_dependencies(root_path: str, module_paths: List[str], search_candidate_paths: List[str], depth: int = sys.maxsize) -> List[str]:
-    search_paths: List[List[str]] = [module_paths]
-    searched_result_paths: List[str] = []
-    current_depth: int = 0  # 探索中の階層の深さを0で初期化
-    logging.info('\n== Parsing module dependencies ==')
-    # 指定された深さまで依存関係を解析する
-    for i in range(0, depth):
-        # 次に探索するファイルのパスを格納するリスト追加する
-        search_paths.append([])
-        # 現在の階層のログを出力する
-        logging.info(f"\nDepth: {current_depth}")
-        # 現在の階層のファイルのパスを取得する
-        for path in search_paths[current_depth]:
-            # 現在の階層のファイルのパスが探索済みのパスに含まれている場合、次のファイルのパスを探索する
-            if path in searched_result_paths or path not in search_candidate_paths:
-                continue
-
-            logging.info(f"  {path}")
-            # 現在の階層のファイルのパスを探索済みのパスの先頭に追加する
-            searched_result_paths.insert(0, path)
-            # 現在の階層のファイルのパスから、依存関係を解析して、ファイルのパスを取得する。この時、絶対パスに変換する
-            dependencies: List[str] = extract_imports(root_path, path)
-            # 現在の階層のファイルのパスの依存関係のうち、探索済みのファイルのパスに含まれていない、かつ、探索候補のファイルのパスに含まれている場合は、次の階層のファイルのパスに追加する
-            for dependency in dependencies:
-                if dependency in searched_result_paths or dependency not in search_candidate_paths:
-                    continue
-
-                search_paths[current_depth + 1].append(dependency)
-        current_depth += 1  # 次の階層に移動する
-        # 次の階層のファイルのパスが存在しない場合、探索を終了する
-        if len(search_paths[current_depth]) == 0:
-            break
-
-    return searched_result_paths
 
 
 def code_split(code: str, max_chara: int = sys.maxsize, max_token: int = sys.maxsize) -> List[str]:
@@ -321,6 +236,108 @@ def count_tokens(text: str, model: str = 'gpt-4') -> int:
     return len(encoding.encode(text))
 
 
+class DependenciesSearcher():
+    def __init__(self, root_path: str, module_paths: List[str], search_candidate_paths: List[str], depth: int = sys.maxsize):
+        self.root_path: str = root_path
+        self.module_paths: List[str] = module_paths
+        self.search_candidate_paths: List[str] = search_candidate_paths
+        self.depth: int = depth
+
+    # 起点となるファイルのパスから、依存関係を解析して、ファイルのパスを返す
+    def search_dependencies(self) -> List[str]:
+        search_paths: List[List[str]] = [self.module_paths]
+        searched_result_paths: List[str] = []
+        current_depth: int = 0  # 探索中の階層の深さを0で初期化
+        logging.info('\n== Parsing module dependencies ==')
+        # 指定された深さまで依存関係を解析する
+        for i in range(0, self.depth):
+            # 次に探索するファイルのパスを格納するリスト追加する
+            search_paths.append([])
+            # 現在の階層のログを出力する
+            logging.info(f"\nDepth: {current_depth}")
+            # 現在の階層のファイルのパスを取得する
+            for path in search_paths[current_depth]:
+                # 現在の階層のファイルのパスが探索済みのパスに含まれている場合、次のファイルのパスを探索する
+                if path in searched_result_paths or path not in self.search_candidate_paths:
+                    continue
+
+                logging.info(f"  {path}")
+                # 現在の階層のファイルのパスを探索済みのパスの先頭に追加する
+                searched_result_paths.insert(0, path)
+                # 現在の階層のファイルのパスから、依存関係を解析して、ファイルのパスを取得する。この時、絶対パスに変換する
+                dependencies: List[str] = self.extract_imports(path)
+                # 現在の階層のファイルのパスの依存関係のうち、探索済みのファイルのパスに含まれていない、かつ、探索候補のファイルのパスに含まれている場合は、次の階層のファイルのパスに追加する
+                for dependency in dependencies:
+                    if dependency in searched_result_paths or dependency not in self.search_candidate_paths:
+                        continue
+
+                    search_paths[current_depth + 1].append(dependency)
+            current_depth += 1  # 次の階層に移動する
+            # 次の階層のファイルのパスが存在しない場合、探索を終了する
+            if len(search_paths[current_depth]) == 0:
+                break
+
+        return searched_result_paths
+
+    def extract_imports(self, relative_path: str) -> List[str]:
+        """
+        指定されたPythonファイルのインポートを解析し、インポートされたモジュールのファイルの相対パスのリストを返します。
+
+        Args:
+            root_path (str): 依存関係を解析する起点のPythonファイルのパス。
+            relative_path (str): 依存関係を解析するPythonファイルのパス。
+
+        Returns:
+            List[str]: インポートされたモジュールのファイルの相対パスのリスト。
+        """
+        # ファイルの内容を読み込む
+        code = read_file(relative_path, remove_comments=False)
+        absolute_path = os.path.join(self.root_path, relative_path)
+        # コードをASTで解析する
+        tree = ast.parse(code)
+
+        # AST内のすべてのImportFromノードを検索する
+        imports: List[ast.ImportFrom] = [node for node in ast.walk(tree) if isinstance(node, ast.ImportFrom)]
+
+        result_module_names = []
+
+        # モジュール名を取得する
+        for node in imports:
+            import_class_and_func_names = []
+            module_name: str = node.module or ''
+
+            # インポートされたクラスや関数の名前を取得する
+            for alias in node.names:
+                import_class_and_func_names.append(alias.name)
+
+            # 相対インポートの場合は、絶対インポートに変換する
+            if node.level > 0:
+                base_dir = os.path.dirname(absolute_path)
+                for _ in range(node.level - 1):
+                    base_dir = os.path.dirname(base_dir)
+
+                # root_pathとbase_dirの共通部分を削除するし、相対パスに変換する
+                package_relative_name = os.path.relpath(base_dir, self.root_path).replace("/", ".")
+                # モジュール名を結合する
+                module_name = f"{package_relative_name}.{module_name}"
+
+            # モジュール名がパッケージであるの場合は、パッケージに含まれるモジュールの内、直接importしているクラスや関数を含むモジュールを取得する
+            if is_package(module_name):
+                modules = get_modules_in_package(module_name)
+                for module in modules:
+                    result_module_names.append(f"{package_relative_name}.{module}")
+                continue
+
+            result_module_names.append(module_name)  # モジュール名を追加する
+
+        # モジュール名をファイルの相対パスに変換する
+        relative_paths = []
+        for module_name in result_module_names:
+            relative_path = module_name.replace(".", "/") + ".py"
+            relative_paths.append(relative_path)
+        return relative_paths
+
+
 def main(root_path: str, module_paths: List[str] = [], depth: int = sys.maxsize, no_comment: bool = False, max_chara: int = sys.maxsize,
          max_token: int = sys.maxsize, excludes: List[str] = []):
     """指定されたファイルのパスのファイルの内容を取得する
@@ -367,7 +384,8 @@ def main(root_path: str, module_paths: List[str] = [], depth: int = sys.maxsize,
     search_candidate_paths: List[str] = exclude_paths(all_py_paths, excludes)
 
     # 起点となるファイルのパスから、依存関係を解析して、ファイルのパスを取得する
-    searched_result_paths: List[str] = search_dependencies(root_path, module_paths, search_candidate_paths, depth)
+    searcher = DependenciesSearcher(root_path, module_paths, search_candidate_paths, depth=depth)
+    searched_result_paths: List[str] = searcher.search_dependencies()
 
     # 依存関係を解析したファイルのパスから、ファイルの内容を取得する
     chunked_content: List[str] = create_content(searched_result_paths, max_chara, max_token, no_comment)
